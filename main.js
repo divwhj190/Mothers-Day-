8f0583500121900c1d8504965a609859c1650ef7
divwhj190 <83068263+divwhj190@users.noreply.github.com>
2022-05-08 11:07:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var x = document.getElementById("myAudio"); 
+var y = document.getElementById("myAudio1"); 
+var z = document.getElementById("myAudio2"); 
+var a = document.getElementById("myAudio3"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function playAudio1() { 
+    y.play(); 
+  }
+
+  function playAudio2() { 
+    z.play(); 
+  }
+  function playAudio3() { 
+    a.play(); 
+  }
